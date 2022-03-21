0d13b81cb1ad14531d2050403737bdf7ae07c9df
AshleyAjuz <45369209+AshleyAjuz@users.noreply.github.com>
2022-03-21 12:46:14 -0400

    Hello World Assignment

---
+++ 1185_LED_blinkOnce.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(0,OUTPUT); // Setting digital pin 0 as output
+
+}
+
+void loop() {
+  digitalWrite(0,HIGH); //LED on
+  delay(1000); //wait for a second
+  digitalWrite(0, LOW); //LED off
+  delay(1000);
+
+}
