52f0d4672b03f50a4402800af158520f8d38b269
Destiny Elings <56138742+destinyelings@users.noreply.github.com>
2022-03-27 16:57:06 -0400

    Add files via upload
    recycle it action detection, detect action on rotary encoder, display to lcd

--- main.ino
+++ main.ino
@@ -1,17 +1,25 @@
+//libraries
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <SoftwareSerial.h>
 
+//pin assignments
 #define audioRx 0
 #define audioTx 1
 #define button 2
-#define rotary 3
+#define rotaryOutputA 5
+#define rotaryOutputB 6
 #define forceResistor 4
-#define LCD_SDA A4
-#define LCD_SCL A5
 
+//library instantiations
+SoftwareSerial mySoftwareSerial(audioRx, audioTx);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+//variables
+boolean rotaryOutputALast;
+boolean rotaryOutputBLast;
+
+//class definitions
 class CompostIt {
   private:
   public:
@@ -20,6 +28,7 @@ class CompostIt {
 class RecycleIt {
   private:
   public:
+  boolean action();
 };
 
 class TrashIt {
@@ -62,22 +71,50 @@ class Commands {
     void checkCommand();
 };
 
-CompostIt compostIt;
-SoftwareSerial mySoftwareSerial(audioRx, audioTx);
-  
+//function definitions
+boolean RecycleIt::action(){
+  boolean outputA = digitalRead(rotaryOutputA);
+  boolean outputB = digitalRead(rotaryOutputB);
+
+  if((outputA != rotaryOutputALast) or (outputB != rotaryOutputBLast))
+    return true;
+  else
+    return false;
+}
+
+//object instantiation
+RecycleIt recycleit;
+
 void setup() {
+  //pin mode setup
   pinMode(button, INPUT);
   pinMode(forceResistor, INPUT);
-  pinMode(rotary, INPUT);
+  pinMode(rotaryOutputA, INPUT);
+  pinMode(rotaryOutputB, INPUT);
 
+  //lcd setup
   lcd.begin(0x27, 16, 2);
   lcd.backlight();
   lcd.clear();
   lcd.setCursor(4,0);
-  lcd.print("DisposeIt  ");
+  lcd.print("DisposeIt");
+
+  //rotary start state
+  digitalWrite(rotaryOutputA, HIGH);
+  digitalWrite(rotaryOutputB, HIGH);
+  rotaryOutputALast = HIGH;
+  rotaryOutputBLast = HIGH;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //polling for recycle
+  boolean recycleItAction = recycleit.action();
 
+  if (recycleItAction) {
+    lcd.clear();
+    lcd.print("Recycled!");
+    delay(1000);
+  }else
+    lcd.clear();
+  
 }
